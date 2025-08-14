46d433774bd64b1d4c2b46b58765c1deecf9a3ce
saloniramteke0209 <saloniramteke2007@gmail.com>
2025-08-14 11:31:05 +0530

    Delete Backend directory

--- Backend/src/router/Reviewrouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getReview, Registercontact } from '../controller/Reviewcontroller.js'
-
-const Reviewrouter = express.Router()
-
-Reviewrouter.post('/', Registercontact)
-Reviewrouter.get('/allcontact', getReview)
-
-export default Reviewrouter
